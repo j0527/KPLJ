ab046526ea3011d48754c648e32ba18342b68097
solia0394@naver.com <15640527adb>
2023-10-21 15:50:39 +0900

    채팅페이지 수정

--- lib/view/chats.dart
+++ lib/view/chats.dart
@@ -5,6 +5,8 @@ class Chats extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Text("귀여운")
+    );
   }
 }
