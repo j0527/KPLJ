1671a8d3cb7a00f3e5e749ad0d5283dbcf71bffc
Jty98 <clclcfnfn@naver.com>
2023-10-22 00:57:50 +0900

    geolocator, permisson_handler ymal추가, plist 수정, 메인페이지 살짝 수정

--- lib/view/mainpage.dart
+++ lib/view/mainpage.dart
@@ -1,6 +1,6 @@
 import 'package:blind_dating/components/imageSlider_widget.dart';
-import 'package:blind_dating/indicatorCurrent_model.dart';
-import 'package:blind_dating/sliderItems_model.dart';
+import 'package:blind_dating/model/indicatorCurrent_model.dart';
+import 'package:blind_dating/model/sliderItems_model.dart';
 import 'package:carousel_slider/carousel_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -14,12 +14,20 @@ class MainPage extends StatelessWidget {
   // 이미지와 텍스트 가지는 더미데이터 리스트
   final List<SliderlItems> carouselItems = [
     SliderlItems(
-      imagePath: 'images/불테리어.png',
-      description: '박명수 27세', 
+      userimagePath: 'images/불테리어.png',
+      userName: '박명수', 
+      userAge: '10세',
+      userLocation: '내 코앞',
+      userDestance: '1m',
+      userMBTI: 'LOVE'
     ),
     SliderlItems(
-      imagePath: 'images/퍼그.png',
-      description: '호날두 7세', 
+      userimagePath: 'images/퍼그.png',
+      userName: '크리스티아누 호날두', 
+      userAge: '27세',
+      userLocation: '평양 직할시',
+      userDestance: '100km',
+      userMBTI: 'ESTJ'
     ),
   ];
 
@@ -33,16 +41,20 @@ class MainPage extends StatelessWidget {
           children: [
             const Padding(
               padding: EdgeInsets.all(20.0),
-              child: Text('오늘의 추천',
+              child: Text(
+              '오늘의 추천',
               style: TextStyle(
                 fontSize: 25,
                 fontWeight: FontWeight.bold
               ),
               ),
             ),
+            // const SizedBox(
+            //   height: 50,
+            // ),
               Container(
-                color: Colors.amber,
-                height: 400, // 이미지 슬라이더 높이
+                color: Color.fromARGB(255, 99, 182, 203),
+                height: 500, // 이미지 슬라이더 영역 크기
                 child: Stack(
                   children: [
                     GetBuilder<IndicatorCurrent>(
@@ -50,14 +62,14 @@ class MainPage extends StatelessWidget {
                         return CarouselSliderWidget(
                           // CarouselSliderWidget위젯에 전달하고 가져오는부분
                           controller: _controller, // 이미지 슬라이더를 제어하는 컨트롤러를 전달
-                          imageList: carouselItems, // 이미지와 설명을 가진 리스트를 전달
+                          userInfoList: carouselItems, // 이미지와 설명을 가진 리스트를 전달
                           current: controller.current, // 현재 페이지를 관리하는 GetX 컨트롤러에서 현재 페이지를 가져옴
                         );
                       },
                     ),
                     CarouselIndicator(
                       // CarouselIndicator위젯에 전달하고 가져오는부분
-                      imageList: carouselItems, // 이미지와 설명을 가진 리스트를 전달
+                      userInfoList: carouselItems, // 이미지와 설명을 가진 리스트를 전달
                       current: indicatorCurrent.current, // GetX 컨트롤러에서 현재 페이지를 가져옴
                       controller: _controller, // 이미지 슬라이더를 제어하는 컨트롤러를 전달
                     // ),
@@ -72,9 +84,6 @@ class MainPage extends StatelessWidget {
                   ],
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(20),
-              )
             ],
           ),
         ),
